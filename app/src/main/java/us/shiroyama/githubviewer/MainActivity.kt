3b4307521086071002a74a4d0e2a94b7c463a814
Fumihiko Shiroyama <shiroyaf@amazon.com>
2020-02-16 11:47:17 +0900

    Initial commit

---
+++ app/src/main/java/us/shiroyama/githubviewer/MainActivity.kt
@@ -0,0 +1,12 @@
+package us.shiroyama.githubviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
